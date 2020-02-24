dc8688802fda8b67290ca74fd3158ed0c766458a
Егорка <gvozdetscky.egor@yandex.ru>
2020-02-24 15:14:40 +0400

    Уроки 13-15

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './Header.module.css';
+
+const Header = () => {
+  return <header className={classes.header}>
+    <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Inkscape.logo.svg/390px-Inkscape.logo.svg.png' />
+  </header>
+}
+
+export default Header
