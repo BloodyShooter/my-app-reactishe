c8f1835bc8dcb95ee8a2f0b75a60270e2caee26b
Егорка <gvozdetscky.egor@yandex.ru>
2020-02-26 00:41:09 +0400

    19-20 урок

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import classes from './Dialogs.module.css'
+
 const Dialogs = (props) => {
     return (
         <div>
-           Dialogs
+            Dialogs
         </div>
     )
 };
