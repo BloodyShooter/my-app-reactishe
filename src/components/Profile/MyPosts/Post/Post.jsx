f38f1e9e7a91f3f7d3a0c8f56b60854d7b9f790f
Егорка <gvozdetscky.egor@yandex.ru>
2020-02-25 19:25:49 +0400

    little update

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import classes from './Post.module.css'
 
-const Post = () => {
+const Post = (props) => {
   return (
     <div className={classes.item}>
       <img src='https://s6.cdn.teleprogramma.pro/wp-content/uploads/2019/11/e95bd1baeb47bb9fb6be787a7741d843.jpg' />
-      post 1
+      <div>
+        {props.message} 
+      </div>
       <div>
         <span>like</span>
       </div>
