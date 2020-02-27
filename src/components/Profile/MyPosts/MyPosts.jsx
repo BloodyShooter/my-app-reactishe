b48fda435c2b638a1c9d3057da8d64b3948bf372
Егорка <gvozdetscky.egor@yandex.ru>
2020-02-27 22:49:11 +0400

    22-23 урок

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,19 +3,21 @@ import classes from './MyPosts.module.css'
 import Post from './Post/Post'
 
 const MyPosts = () => {
-  return (
-    <div>
-      my posts
-      <div>
-        <textarea></textarea>
-        <button>Add post</button>
-      </div>
-      <div className={classes.posts}>
-        <Post message='Hi, how are you?'/>
-        <Post message="It's my first post"/>
-      </div>
-    </div>
-  )
+    return (
+        <div className={classes.postBlock}>
+            <h3>my posts</h3>
+            <div>
+                <div><textarea></textarea></div>
+                <div>
+                    <button>Add post</button>
+                </div>
+            </div>
+            <div className={classes.posts}>
+                <Post message='Hi, how are you?'/>
+                <Post message="It's my first post"/>
+            </div>
+        </div>
+    )
 }
 
 export default MyPosts
