bcd9a6632bc0a49040e9fc76085ad71f8e0dfa99
Егорка <gvozdetscky.egor@yandex.ru>
2020-05-02 23:31:10 +0400

    71 урок локальный state - крутая вещь

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import classes from './ProfileInfo.module.css'
 import Preloader from "../../common/preloader/Preloader";
+import ProfileStatus from "./ProfileStatus"
 
 const ProfileInfo = (props) => {
 
@@ -10,12 +11,13 @@ const ProfileInfo = (props) => {
 
     return (
         <div>
-            <div>
-                <img src='https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg'/>
-            </div>
+            {/*<div>*/}
+            {/*    <img src='https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg'/>*/}
+            {/*</div>*/}
             <div className={classes.descriptionBlock}>
                 <img src={props.profile.photos.large} />
                 {props.profile.aboutMe}
+                <ProfileStatus status="Hello My Friends"/>
             </div>
         </div>
     )
